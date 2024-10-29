842ff46f33e570436fc959e2faef0c9be4b55d24
wangwang2004 <wang20041116@gmail.com>
2024-10-29 18:02:58 +0800

    113-10-29

---
+++ 113-10-29/Tutorial 4-2/Payroll with Overtime/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_with_Overtime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal hoursWorked, hourlyPayRate, grossPay;
+            const decimal BASEHOURS = 40m;
+            const decimal OVERTIME = 1.5m;
+            try
+            {
+                hoursWorked = decimal.Parse(hoursWorkedTextBox.Text);
+                hourlyPayRate = decimal.Parse(hourlyPayRateTextBox.Text);
+
+
+                if (hoursWorked > BASEHOURS)
+                {
+                    grossPay = BASEHOURS * hourlyPayRate +
+                        ((hoursWorked - BASEHOURS) * hourlyPayRate * OVERTIME);
+                }
+                else
+                {
+                    grossPay = hoursWorked * hourlyPayRate;
+                }
+                grossPayLabel.Text = grossPay.ToString("c");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the TextBoxes and gross pay label.
+            hoursWorkedTextBox.Text = "";
+            hourlyPayRateTextBox.Text = "";
+            grossPayLabel.Text = "";
+
+            // Reset the focus.
+            hoursWorkedTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
---
+++ 113-10-29/Tutorial 4-3/Loan Qualifier/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loan_Qualifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            decimal salary;
+            int years;
+            const int MINYEARS = 2;
+
+            try
+            {
+                salary = decimal.Parse(salaryTextBox.Text);
+                years = int.Parse(yearsTextBox.Text);
+
+                if (salary >= MINYEARS)
+                {
+                    decisionLabel.Text = "條件符合，貸款通過";
+                }
+                else
+                {
+                    decisionLabel.Text = "薪資所得未達標準，貸款不通過";
+                }
+            }
+            else
+            {
+                decisionLabel.Text = "薪資所得未達標準，貸款不通過";
+            }
+       
+        }
+        catch(Exception ex)
+            
+           
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the TextBoxes and the decisionLabel.
+            salaryTextBox.Text = "";
+            yearsTextBox.Text = "";
+            decisionLabel.Text = "";
+
+            // Reset the focus.
+            salaryTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
