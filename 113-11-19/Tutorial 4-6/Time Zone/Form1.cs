bc3761b75af514715595041cd2099c75bab119b4
wangwang2004 <wang20041116@gmail.com>
2024-11-19 18:06:44 +0800

    113-11-19

---
+++ 113-11-19/Tutorial 4-6/Time Zone/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Time_Zone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            string city;
+            if(cityListBox.SelectedIndex != -1)
+            {
+                city = cityListBox.SelectedItem.ToString();
+
+                switch (city) 
+                {
+                    case "Denver":
+                        timeZoneLabel.Text = "Mountain";
+                        break;
+                    case "Honolulu":
+                        timeZoneLabel.Text = "Hawaii-Aleutian";
+                        break;
+                    case "Minneapolis":
+                        timeZoneLabel.Text = "Hawaii-Aleutian";
+                        break;
+
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("選擇一個城市");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
