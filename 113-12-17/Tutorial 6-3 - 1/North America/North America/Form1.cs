271588903521dd01c5f24afaa490f581f2773610
wangwang2004 <wang20041116@gmail.com>
2024-12-17 17:54:11 +0800

    113-12-17

---
+++ 113-12-17/Tutorial 6-3 - 1/North America/North America/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace North_America
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The GetFileName method gets a filename from the
+        // user and assigns it to the variable passed as
+        // an argument.
+
+        // The GetCountries method accpets a filename as an
+        // argument. It opens the specified file and displays
+        // its contents in the countriesListBox control.
+        
+
+        private void getCountriesButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+    }
+}
